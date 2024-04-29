831b8724d2e263be0a2466e23382ba2bd6369606
Bhagyashrirs <64522583+Bhagyashrirs@users.noreply.github.com>
2024-04-29 18:39:32 +0530

    Update setupTestEnv.js

--- scripts/setupTestEnv.js
+++ scripts/setupTestEnv.js
@@ -9,7 +9,7 @@ const {fs} = require('aql-utils');
 
         const envJsonContent = {
             test : {
-                db      : 'mongodb://mongo:27017/test',
+                db      : 'mongodb://aquilacms-server:d3yayXB135vqK6bTaYGsJTLiJthyIB7B29BlN82YcawqP8iCjmDQpEpkRRxsF6jLCY8NDkqPIAPKACDbQsr0bQ==@aquilacms-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@aquilacms-server@',
                 devMode : {
                     mailTo  : 'testaquila@yopmail.com',
                     compile : false,
